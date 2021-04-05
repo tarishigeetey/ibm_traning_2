7ad67690d658a06b519b78275c194edd506cb1cc
Tarishi Geetey <tgeeteyt@gmail.com>
2021-04-05 09:44:44 +0530

    Commit to add mvn_math

---
+++ mvn-math/src/main/java/com/ibm/math/Maths.java
@@ -0,0 +1,19 @@
+package com.ibm.math;
+
+public class Maths {
+	
+	public Maths() {
+		// TODO Auto-generated constructor stub
+	}
+	public int sum(int a, int b) {
+		return a+b;
+	}
+	
+	public int subs(int a, int b) {
+		return a-b;
+	}
+	
+	public int div(int a, int b) {
+		return a/b;
+	}
+}
---
+++ mvn-math/src/test/java/com/ibm/math/TestMaths.java
@@ -0,0 +1,38 @@
+package com.ibm.math;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class TestMaths {
+	
+	private static Maths maths;
+	
+	@BeforeAll
+	public static void setup() {
+		maths = new Maths();
+	}
+	
+	@Test
+	public void testAdd() {
+		assertEquals(10, maths.sum(5,5));
+	}
+	
+	@Test
+	public void testSub() {
+		assertEquals(10, maths.subs(15,5));
+	}
+	
+	@Test
+	public void testDiv() {
+		assertEquals(10, maths.div(10,1));
+	}
+	
+	@Test
+	public void testZeroDiv() {
+		assertThrows(ArithmeticException.class, () -> maths.div(2, 0));
+	}
+
+}
